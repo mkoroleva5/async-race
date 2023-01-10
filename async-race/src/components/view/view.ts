2ecc514c4d89d4f035ef9299d5dcdae4a3ef1d35
mkoroleva5 <mkoroleva5@gmail.com>
2023-01-10 23:38:25 +0300

    feat: create view options

---
+++ async-race/src/components/view/view.ts
@@ -0,0 +1,38 @@
+import { getLS, setLS } from '../../utils/localStorageHelpers';
+import './view.css';
+
+/* global document */
+export const createViewOptions = () => {
+  const view = document.createElement('div');
+  view.className = 'view-wrapper';
+  const options = ['Garage', 'Winners'];
+
+  options.map((el) => {
+    const viewOption = document.createElement('div');
+    const viewInput = document.createElement('input');
+    const viewLabel = document.createElement('label');
+    viewOption.className = 'view-option';
+    viewInput.className = 'view-input';
+    viewLabel.className = 'view-label';
+
+    viewInput.setAttribute('type', 'radio');
+    viewInput.setAttribute('name', 'view');
+    viewInput.setAttribute('id', el);
+    viewLabel.setAttribute('for', el);
+    viewLabel.innerHTML = el;
+
+    viewInput.addEventListener('input', () => {
+      setLS('view', el);
+    });
+
+    if (!getLS('view')) setLS('view', 'Garage');
+    if (getLS('view') === el) viewInput.checked = true;
+
+    viewOption.appendChild(viewInput);
+    viewOption.appendChild(viewLabel);
+    view.appendChild(viewOption);
+    return view;
+  });
+
+  return view;
+};
