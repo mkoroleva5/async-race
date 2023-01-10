fd3ec5bdfc86a458499da06d7010a99028ad5532
mkoroleva5 <mkoroleva5@gmail.com>
2023-01-10 23:40:43 +0300

    feat: add header to main page

--- async-race/src/main.ts
+++ async-race/src/main.ts
@@ -1,1 +1,8 @@
+import { createHeader } from './components/header/header';
 import './style.css';
+
+/* global document */
+const body = document.querySelector('body') as HTMLBodyElement;
+const header = createHeader();
+
+body.appendChild(header);
