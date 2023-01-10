d9dd017a79236e0fdd4b335ff2e85ed653dfc39f
mkoroleva5 <mkoroleva5@gmail.com>
2023-01-10 23:38:54 +0300

    feat: create localStorage helper functions

---
+++ async-race/src/utils/localStorageHelpers.ts
@@ -0,0 +1,12 @@
+/* eslint-disable no-undef */
+export const setLS = (key: string, value: unknown) => {
+  localStorage.setItem(key, JSON.stringify(value));
+};
+
+export const getLS = (key: string) => {
+  const value = localStorage.getItem(key);
+  if (value) {
+    return JSON.parse(value);
+  }
+  return null;
+};
