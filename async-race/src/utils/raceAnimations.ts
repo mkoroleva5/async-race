b51d8299a20d94360d346626fedb6f2a3f4b8787
mkoroleva5 <mkoroleva5@gmail.com>
2023-01-23 19:44:34 +0300

    feat: add race animations

---
+++ async-race/src/utils/raceAnimations.ts
@@ -0,0 +1,68 @@
+import { createWinner, getWinner, getWinners, setDrive, stopRace } from '../components/api';
+import { state } from '../components/store';
+
+export const startRaceAnimation = async (id: number, time: number) => {
+  const startButton = document.querySelector(`.start-button-${id}`) as HTMLButtonElement;
+  startButton.disabled = true;
+  startButton.style.backgroundColor = '#c9c9c9';
+
+  const stopButton = document.querySelector(`.stop-button-${id}`) as HTMLButtonElement;
+  stopButton.disabled = false;
+  stopButton.style.backgroundColor = 'var(--bg-color)';
+
+  const car = document.querySelector(`.car-${id}`) as HTMLElement;
+
+  car.addEventListener('animationend', async () => {
+    try {
+      await createWinner({ id: state.currentWinner.winner?.id, wins: 1, time: time }); // ---------- только 1-го
+    } catch (err) {
+      console.log(err);
+    }
+
+    if (!state.currentWinner) {
+      state.currentWinner = { winner: state.cars.find((el) => el.id === id), time };
+      const winnerPopup = document.querySelector('.winner-popup-wrapper');
+      winnerPopup.style.display = 'flex';
+    }
+  });
+
+  car.style.animationDuration = `${time}ms`;
+  car.classList.add('race');
+  car.style.animationPlayState = 'running';
+
+  try {
+    await setDrive(id);
+  } catch (err) {
+    console.log(err);
+    car.style.animationPlayState = 'paused';
+    car.classList.add('rotate');
+  }
+
+  stopButton.disabled = true;
+  stopButton.style.backgroundColor = '#c9c9c9';
+  startButton.disabled = false;
+  startButton.style.backgroundColor = 'var(--bg-color)';
+};
+
+export const stopRaceAnimation = async (id: number) => {
+  const stopButton = document.querySelector(`.stop-button-${id}`) as HTMLButtonElement;
+  stopButton.disabled = true;
+  stopButton.style.backgroundColor = '#c9c9c9';
+
+  const startButton = document.querySelector(`.start-button-${id}`) as HTMLButtonElement;
+  startButton.disabled = false;
+  startButton.style.backgroundColor = 'var(--bg-color)';
+
+  const car = document.querySelector(`.car-${id}`) as HTMLElement;
+
+  car.style.animationPlayState = 'paused';
+  car.classList.remove('race');
+  car.classList.remove('rotate');
+};
+
+export const resetRaceAnimation = (id: number) => {
+  const car = document.querySelector(`.car-${id}`) as HTMLElement;
+
+  car.classList.remove('race');
+  car.classList.remove('rotate');
+};
